03f11446f10920505b9b8a43c7de9359ce58fd16
wangjunqian <1578046487@qq.com>
2018-12-13 21:53:21 +0800

    1

--- app/src/main/java/com/example/broadcastbestpractice/MainActivity.java
+++ app/src/main/java/com/example/broadcastbestpractice/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button forceOffline = (Button) findViewById(R.id.force_offline);
-        forceOffline.setOnClickListener(new View.OnClickListener() {
+            forceOffline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
